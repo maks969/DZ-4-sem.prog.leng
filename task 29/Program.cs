cfee5afbf829032b7f989497559376785216f0d5
maks969 <111703818+maks969@users.noreply.github.com>
2022-09-30 21:59:59 +0300

    29 end

---
+++ task 29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+
+Console.WriteLine("Введите число");
+string str = Console.ReadLine()!;
+var count = str.Length!;
+var array = new int[count];
+for (int i = 0; i < count; i++)
+{
+    array[i] = int.Parse(str[i].ToString());
+    Console.Write($" {array[i]},");
+}
