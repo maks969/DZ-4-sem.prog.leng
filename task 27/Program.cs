26f5723276f6333bc9b24f8bea134446dc5b6c5a
maks969 <111703818+maks969@users.noreply.github.com>
2022-09-30 21:43:58 +0300

    27 end

---
+++ task 27/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число");
+string str = Console.ReadLine()!;
+var count = str.Length!;
+var array = new int[count];
+for (int i = 0; i < count; i++)
+{
+    array[i] = int.Parse(str[i].ToString());
+}
+int rez = array.Sum();
+Console.WriteLine(rez);
