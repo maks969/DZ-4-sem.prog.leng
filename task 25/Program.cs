ba36d8dea96ba455e193e741063fead4042134fe
maks969 <111703818+maks969@users.noreply.github.com>
2022-09-30 17:53:25 +0300

    25 end

---
+++ task 25/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B)
+ и возводит число A в натуральную степень B.
+        пример: 3, 5 -> 243 (3⁵) */
+
+
+Console.WriteLine("Введите А (число которое возводим)");
+double number = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите B (степень числа)");
+double power = double.Parse(Console.ReadLine()!);
+
+
+double exponent = number;
+
+for (int i = 1; i < power; i++)
+{
+    exponent = exponent * number;
+}
+Console.WriteLine($"число A в степени B = {exponent}");
