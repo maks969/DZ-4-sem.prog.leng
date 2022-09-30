06359c223d655e991fd0e45273d82dddcecd07b3
maks969 <111703818+maks969@users.noreply.github.com>
2022-09-30 23:55:36 +0300

    end

--- task 29/Program.cs
+++ task 29/Program.cs
@@ -4,7 +4,7 @@ int[] A = new int[8];
 
   for (int i = 0; i < 8; i++)
     {
-      Console.WriteLine("Введите число");
+      Console.WriteLine($"Введите число № {i+1} и нажмите 'ентер'");
       A[i] = int.Parse(Console.ReadLine()!);
     }
 Console.WriteLine("[{0}]", string.Join(", ", A));
